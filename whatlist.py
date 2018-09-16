3cffbbabcaa9ec0c79236f18a4142c1431078255
zavidova <inklover29>
2018-09-16 19:19:11 +0200

    what extraction

--- whatlist.py
+++ whatlist.py
@@ -1,13 +1,15 @@
 import csv
 import time
 
-search_string = "what"
 
-with open('catalog.csv', 'rb') as f, open('whatlist.csv', 'wb') as g:
-    reader = csv.DictReader(f)
-    c1, c2, c3, *_ = reader.fieldnames
 
-    writer = csv.DictWriter(g, fieldnames=(c1, c2))
-    for row in reader:
-        if row[c3] == search_string:
-            writer.writerow({c1:row[c1], c2:row[c2]})
+import re
+
+songs = open("catalog.csv", "r")
+what = open("whatlist.csv", "w")
+
+for line in songs:
+    if re.match("(.*)(W|w)hat(.*)", line):
+        print >> what, line,
+            
+
