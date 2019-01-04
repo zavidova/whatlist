57c8cd773c6669898f8f4bcc7389b4838db57d0a
zavidova <inklover29>
2019-01-04 17:47:41 +0100

    added first part of the csv

--- whatlist.py
+++ whatlist.py
@@ -11,5 +11,13 @@ what = open("whatlist.csv", "w")
 for line in songs:
     if re.match("(.*)(W|w)hat(.*)", line):
         print >> what, line,
-            
 
+-----second script
+
+import csv
+
+with open('final_what.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if row['artist'] == 'Britney Spears':
+            print(row['title'] ,row ['artist'])
