7614c9d744e501f528638037b90df41923056a02
zavidova <inklover29>
2019-01-04 17:50:40 +0100

    commented python

--- whatlist.py
+++ whatlist.py
@@ -1,23 +1,13 @@
 import csv
 import time
 
-
-
 import re
 
-songs = open("catalog.csv", "r")
-what = open("whatlist.csv", "w")
+songs = open("2.csv", "r")
+what = open("part_02.csv", "w")
 
 for line in songs:
     if re.match("(.*)(W|w)hat(.*)", line):
         print >> what, line,
 
------second script
-
-import csv
-
-with open('final_what.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        if row['artist'] == 'Britney Spears':
-            print(row['title'] ,row ['artist'])
+#this script uses regex, if you forget what it is go to your message at H&D slack tech-support channel -k
